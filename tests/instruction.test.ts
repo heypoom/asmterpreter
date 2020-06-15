41183fb33b36d16a129d170e317c9c763855f163
Phoomparin Mano <phoomparin@gmail.com>
2020-06-15 22:20:22 +0700

    :white_check_mark: Add unit tests for pure instruction functions

--- tests/instruction.test.ts
+++ tests/instruction.test.ts
@@ -1,5 +1,38 @@
+import {Machine} from '../src/modules/Machine'
+import {mov, xor, get, push, pop, jmp} from '../src/modules/Instructions'
+
+const m = () => Machine()
+
 describe('instructions', () => {
-  it('should xor 2 numbers', () => {
-    expect(200 ^ 200).toBe(0)
+  it('should be able to move value into a register', () => {
+    const s = mov(m(), 'eax', 0xdead)
+    expect(s.registers.eax).toBe(0xdead)
+  })
+
+  it('should be able to xor itself and return zero', () => {
+    let s = mov(m(), 'eax', 0xdead)
+    s = xor(s, 'eax', get(s, 'eax'))
+
+    expect(s.registers.eax).toBe(0)
+  })
+
+  it('should be able to push value to the stack', () => {
+    let s = mov(m(), 'esp', 0x5)
+    s = push(s, 0xbeef)
+
+    expect(s.memory[5]).toBe(0xbeef)
+  })
+
+  it('should be able to pop value from the stack', () => {
+    let s = mov(m(), 'esp', 0x5)
+    s = push(s, 0xbeef)
+    s = pop(s, 'eax')
+
+    expect(s.registers.eax).toBe(0xbeef)
+  })
+
+  it('should be able to jump by modifying instruction pointer', () => {
+    let s = jmp(m(), 0x20)
+    expect(s.registers.eip).toBe(0x20)
   })
 })
